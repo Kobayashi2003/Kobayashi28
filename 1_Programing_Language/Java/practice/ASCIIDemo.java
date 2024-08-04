de14021ee959b6e1d2be4518db66d201a3058ce5
Efrain Jack <1731237095@qq.com>
2024-08-04 17:10:40 +0800

    240804

---
+++ 1_Programing_Language/Java/practice/ASCIIDemo.java
@@ -0,0 +1,11 @@
+public class ASCIIDemo {
+    public static void main(String[] args) {
+        System.out.println('a' + 10); 
+        System.out.println('A' + 10);
+        System.out.println('0' + 9);
+        
+        System.out.println(0B10);
+        System.out.println(010);
+        System.out.println(0X10);
+    }
+}
---
+++ 1_Programing_Language/Java/practice/LiteralDemo.java
@@ -0,0 +1,26 @@
+public class LiteralDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        int x = 10;
+        int y = 20;
+        System.out.println(x + y);
+
+        double d1 = 10.0;
+        double d2 = 20.0;
+        System.out.println(d1 + d2);
+
+        String s1 = "10";
+        String s2 = "20";
+        System.out.println(s1 + s2);
+
+        char c1 = 'a';
+        char c2 = 'b';
+        System.out.println(c1 + c2);
+
+        boolean b1 = true;
+        boolean b2 = false;
+        System.out.println(b1);
+        System.out.println(b2);
+    }
+}
