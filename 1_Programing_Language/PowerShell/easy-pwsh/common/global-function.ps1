1c74c637edd6514c339909166aec8346c1b2b46d
Efrain Jack <1731237095@qq.com>
2024-08-14 00:28:02 +0800

    240814

--- 1_Programing_Language/PowerShell/easy-pwsh/common/global-function.ps1
+++ 1_Programing_Language/PowerShell/easy-pwsh/common/global-function.ps1
@@ -179,3 +179,22 @@ function global:which {
 
     (Get-Command $Args[0] -ErrorAction SilentlyContinue).Source
 }
+
+
+function global:rename {
+
+<#
+.SYNOPSIS
+    Renames a file
+.EXAMPLE
+    PS> rename C:\temp\foo.txt C:\temp\bar.txt
+#>
+
+    param(
+        [Parameter(Mandatory)]
+        [string]$From,
+        [Parameter(Mandatory)]
+        [string]$To
+    )
+    Rename-Item -Path $From -NewName $To
+}
