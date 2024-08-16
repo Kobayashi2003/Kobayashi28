1a519bf1766c9f8218a46c4cbff123d970c1c7e7
Efrain Jack <1731237095@qq.com>
2024-08-16 16:24:50 +0800

    240816

---
+++ 1_Programing_Language/PowerShell/easy-pwsh/utils/jable-downloader.ps1
@@ -0,0 +1,48 @@
+﻿<#
+.SYNOPSIS
+    Download from Jable
+
+.PARAMETER URL
+    Jable Vedio URl
+.PARAMETER path
+    Vedio save path
+
+.EXAMPLE
+    jable-download -url https://jable.tv/videos/ipx-660/ -path C:\Users\user\Downloads
+#>
+
+
+param(
+    [Parameter(Mandatory=$true)]
+    [ValidatePattern('.*jable.tv/videos/.*/')]
+    [string[]] $url,
+    [Parameter(Mandatory=$false)]
+    [string] $path
+)
+
+$cur_dir = Get-Location
+$script_path = Split-Path $MyInvocation.MyCommand.Path
+$jable_dowloader = Join-Path -Path $script_path -ChildPath "jable-downloader"
+
+if (-not (Test-Path -Path $jable_dowloader)) {
+    Write-Host "Please put jable-downloader in the same directory with this script" -ForegroundColor Red
+    exit 1
+}
+
+if (-not $path) {
+    $path = $cur_dir
+}
+
+if (-not (Test-Path -Path $path)) {
+    New-Item -Path $path -ItemType Directory | Out-Null
+}
+
+$abs_path = Resolve-Path -Path $path
+
+Set-Location $jable_dowloader
+
+& pixi run python ./jable-downloader.py $url -p $abs_path
+
+Set-Location $cur_dir
+
+
---
+++ 1_Programing_Language/Python/Practice/Prac_121To200/prc_126_jable_downloader/jable_downloader.py
@@ -0,0 +1,219 @@
+import tqdm
+import argparse
+
+import os
+import re
+import copy
+import subprocess
+
+from functools import partial
+from concurrent import futures
+
+import m3u8
+from Crypto.Cipher import AES
+
+import requests
+import urllib.request
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def downloadM3U8(url, folderPath):
+
+    """
+    .SYNOPSIS
+        Download M3U8 file from the given url, and get all the ts urls from the m3u8 file.
+        Also, make a decryption object for the ts files if needed.
+
+    .PARAMETER url
+        The url of the jable video you want to download.
+    .PARAMETER folderPath
+        The path of the folder to save the downloaded files.
+
+    .EXAMPLE
+        downloadM3U8('https://jable.tv/videos/mukc-032/', 'D:/Temp') 
+    """
+
+    # set the selenium driver 
+    options = Options() 
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--headless')
+    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36')
+    driver = webdriver.Chrome(options=options)
+    driver.get(url=url)
+
+    # get the m3u8 url of the jable video
+    m3u8urls = re.findall("https://.+m3u8", driver.page_source)
+    m3u8url = m3u8urls[0]
+
+    # download the m3u8 file
+    if not os.path.exists(folderPath):
+        os.makedirs(folderPath)
+    m3u8file = os.path.join(folderPath, f'{url.split("/")[-2]}.m3u8')
+    urllib.request.urlretrieve(m3u8url, m3u8file)
+    # load the m3u8 file
+    m3u8obj = m3u8.load(m3u8file)
+    downloadUrl = '/'.join(m3u8url.split('/')[:-1])
+
+    # get the m3u8 uri and iv
+    m3u8uri = ''                        # uri: uniform resource identifier
+    m3u8iv = ''                         # iv: initialization vector
+    for key in m3u8obj.keys:
+        if key:
+            m3u8uri = key.uri
+            m3u8iv = key.iv
+
+    # make a decryption object
+    if m3u8uri:                         # ci: cipher instance
+        m3u8keyUrl = downloadUrl + '/' + m3u8uri
+        m3u8key = requests.get(m3u8keyUrl, headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36',
+        }, timeout=10).content
+        iv = m3u8iv.replace("0x", "")[:16].encode()
+        ci = AES.new(m3u8key, AES.MODE_CBC, iv) 
+    else:
+        ci = ''                     
+
+    # get all the ts urls 
+    tsUrls = []                         # ts: transport stream
+    for seg in m3u8obj.segments:
+        tsUrls.append(downloadUrl + '/' + seg.uri)
+
+    os.remove(m3u8file)
+    if not os.listdir(folderPath):
+        os.rmdir(folderPath)
+
+    return tsUrls, ci 
+
+
+def downloadTS(downloadList, ci, folderPath, tsUrl):
+    fileName = tsUrl.split('/')[-1][0:-3]
+    saveName = os.path.join(folderPath, fileName + ".ts")
+    if os.path.exists(saveName):
+        # skip downloaded ts
+        print(f'Targe {fileName} already exists, skip. Remaining {len(downloadList)}')
+        downloadList.remove(tsUrl)
+    else:
+        response = requests.get(tsUrl, headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.97 Safari/537.36',
+        }, timeout=10)
+        if response.status_code == 200:
+            content_ts = response.content
+            if ci:
+                content_ts = ci.decrypt(content_ts)
+            with open(saveName, 'wb') as f:
+                f.write(content_ts)
+            downloadList.remove(tsUrl)
+            print(f'Download {fileName} success. Remaining {len(downloadList)}')
+        else:
+            print(f'Download {fileName} failed. Remaining {len(downloadList)}')
+
+
+def downloadTSList(tsUrls, ci, folderPath):
+    if not os.path.exists(folderPath):
+        os.makedirs(folderPath)
+
+    downloadList = copy.deepcopy(tsUrls)
+
+    while downloadList:
+        with futures.ThreadPoolExecutor(max_workers=12) as executor:
+            executor.map(partial(downloadTS, downloadList, ci, folderPath), tsUrls)
+
+
+def mergeTSFiles(tsUrls, tsFolderPath, savePath):
+
+    with open(savePath, 'wb') as f:
+        for ts in tqdm.tqdm(tsUrls):
+            tsName = ts.split('/')[-1][0:-3]
+            tsPath = os.path.join(tsFolderPath, tsName + ".ts")
+            with open(tsPath, 'rb') as f1:
+                f.write(f1.read())
+
+    for ts in tsUrls:
+        tsName = ts.split('/')[-1][0:-3]
+        tsPath = os.path.join(tsFolderPath, tsName + ".ts")
+        os.remove(tsPath)
+    if not os.listdir(tsFolderPath):
+        os.rmdir(tsFolderPath)
+
+
+def downloadCover(url, folderPath):
+
+    # set the selenium driver 
+    options = Options() 
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--disable-extensions')
+    options.add_argument('--headless')
+    options.add_argument('user-agent=Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.125 Safari/537.36')
+    driver = webdriver.Chrome(options=options)
+    driver.get(url=url)
+
+    if not os.path.exists(folderPath):
+        os.mkdir(folderPath)
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    cover_name = soup.find('meta', property='og:image')['content'].split('/')[-1]
+    cover_path = os.path.join(folderPath, cover_name)
+    urllib.request.urlretrieve(soup.find('meta', property='og:image')['content'], cover_path)
+
+
+def isJableVideoUrl(url):
+    if re.match(r'.*jable.tv/videos/.*/', url):
+        return True
+    else:
+        return False
+
+
+def test():
+    # https://jable.tv/videos/mukc-032/
+    url = input('url: ')
+    if not isJableVideoUrl(url):
+        print('Invalid jable video path')
+        exit(1) 
+
+    folderPath = input('folderPath: ')
+    if not folderPath:
+        folderPath = os.getcwd()
+    if not os.path.exists(folderPath):
+        os.makedirs(folderPath)
+
+    m3u8FloderPath  = os.path.join(folderPath, 'm3u8')
+    tsFloderPath    = os.path.join(folderPath, 'ts')
+    videoFilePath   = os.path.join(folderPath, url.split('/')[-2])
+
+    tsUrls, ci = downloadM3U8(url, m3u8FloderPath)
+    downloadTSList(tsUrls, ci, tsFloderPath)
+    mergeTSFiles(tsUrls, tsFloderPath, videoFilePath)
+    
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', type=str, nargs='+', default=None, help='the url of the jable video you want to download')
+    parser.add_argument('-p', '--folderPath', type=str, default=None, help='the path of the folder to save the downloaded files')
+
+    args = parser.parse_args()
+
+    for url in args.url:
+        if not isJableVideoUrl(url):
+            print('Invalid jable video path')
+            exit(1) 
+
+        if not args.folderPath:
+            args.folderPath = os.getcwd()
+        if not os.path.exists(args.folderPath):
+            os.makedirs(args.folderPath)
+
+        # coverFolderPath = os.path.join(args.folderPath, 'cover')
+        coverFolderPath = args.folderPath
+        m3u8FloderPath  = os.path.join(args.folderPath, 'm3u8')
+        tsFloderPath    = os.path.join(args.folderPath, 'ts')
+        videoFilePath   = os.path.join(args.folderPath, url.split('/')[-2] + '.mp4')
+
+        downloadCover(url, coverFolderPath)
+        tsUrls, ci = downloadM3U8(url, m3u8FloderPath)
+        downloadTSList(tsUrls, ci, tsFloderPath)
+        mergeTSFiles(tsUrls, tsFloderPath, videoFilePath)
