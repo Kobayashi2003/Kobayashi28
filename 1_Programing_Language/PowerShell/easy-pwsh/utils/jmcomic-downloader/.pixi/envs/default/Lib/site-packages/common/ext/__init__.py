9da00a5771fe9f29177bcb1687d53f438f9791ed
KOBAYASHI <example@gmail.com>
2024-09-08 17:03:01 +0800

    240908

--- 1_Programing_Language/PowerShell/easy-pwsh/utils/jmcomic-downloader/.pixi/envs/default/Lib/site-packages/common/ext/__init__.py
+++
@@ -1,3 +0,0 @@
-from .cookie_parser import CookieParser, ChromePluginCookieParser
-from .qq_email import QQEmailPostman, EmailConfig
-from .iphone_client import IphoneClient
--- 1_Programing_Language/PowerShell/easy-pwsh/utils/jmcomic-downloader/.pixi/envs/default/Lib/site-packages/plugin_jm_server/__init__.py
+++
@@ -1,3 +0,0 @@
-__version__ = '0.1.23'
-
-from .app import *
