d3ec5b0ba12ef315acc8f551f32053e2984a4da8
kobayashi-labtop <1731237095@qq.com>
2025-01-28 23:11:07 +0800

    2025-01-28-23-11-05

--- 1_Programing_Language/PowerShell/easy-pwsh/core/init.ps1
+++ 1_Programing_Language/PowerShell/easy-pwsh/core/init.ps1
@@ -1,6 +1,5 @@
 ﻿$global:CURRENT_SCRIPT_DIRECTORY = Split-Path (Split-Path $MyInvocation.MyCommand.Definition)
 
-Get-ChildItem (Join-Path -Path $global:CURRENT_SCRIPT_DIRECTORY -ChildPath "common\*ps1" ) | ForEach-Object { & $_.FullName }
 Get-ChildItem (Join-Path -Path $global:CURRENT_SCRIPT_DIRECTORY -ChildPath "core\*ps1"   ) | ForEach-Object { if ($_.Name -ne $MyInvocation.MyCommand.Name) { & $_.FullName } }
 Get-ChildItem (Join-Path -Path $global:CURRENT_SCRIPT_DIRECTORY -ChildPath "start\*ps1"  ) | ForEach-Object { & $_.FullName }
 
---
+++ 1_Programing_Language/PowerShell/easy-pwsh/utils/check-admin.ps1
@@ -0,0 +1,15 @@
+﻿function Check-Admin {
+<#
+.SYNOPSIS
+    Checks if the current console is elevated
+.EXAMPLE
+    if (Check-Admin) { ... }
+#>
+    $admin = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    if ($admin.IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator") -eq $false) {
+        return $false
+    }
+    return $true
+}
+
+check-admin
